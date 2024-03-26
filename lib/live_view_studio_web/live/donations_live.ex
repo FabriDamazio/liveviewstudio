7327d4bf4170b46c2040eda58b8f22c02692c5b2
Fabricio Damazio <fabridamazio@gmail.com>
2024-03-26 15:00:04 -0300

    Class 12 - Sorting

--- lib/live_view_studio_web/live/donations_live.ex
+++ lib/live_view_studio_web/live/donations_live.ex
@@ -4,13 +4,56 @@ defmodule LiveViewStudioWeb.DonationsLive do
   alias LiveViewStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
-
-    socket =
-      assign(socket,
-        donations: donations
-      )
-
-    {:ok, socket}
+    {:ok, socket, temporary_assigns: [donations: []]}
   end
+
+  def handle_params(params, uri, socket) do
+    sort_by = valid_sort_by(params)
+    sort_order = valid_sort_order(params)
+
+    options = %{sort_by: sort_by, sort_order: sort_order}
+    donations = Donations.list_donations(options)
+
+    {:noreply, assign(socket,
+      donations: donations,
+      options: options
+    )}
+  end
+
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+  slot :inner_block, required: true
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"}>
+      <%= render_slot(@inner_block) %>
+      <%= sort_indicator(@sort_by, @options) %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(:asc), do: :desc
+  defp next_sort_order(:desc), do: :asc
+
+  defp sort_indicator(column, %{sort_by: sort_by, sort_order: sort_order}) when column == sort_by do
+    case sort_order do
+      :asc -> "👆"
+      :desc -> "👇"
+    end
+  end
+
+  defp sort_indicator(_, _), do: ""
+
+  defp valid_sort_by(%{"sort_by" => sort_by}) when sort_by in ~w(item quantity days_until_expires) do
+    String.to_atom(sort_by)
+  end
+
+  defp valid_sort_by(_params), do: :id
+
+  defp valid_sort_order(%{"sort_order" => sort_order}) when sort_order in ~w(asc desc) do
+    String.to_atom(sort_order)
+  end
+
+  defp valid_sort_order(_params), do: :asc
 end
